be14d5313d63a73705298274b4b1c8469f98e442
Alexander Hoffmann <alexander.hoffmann@artificial-solutions.com>
2020-04-09 15:07:45 +0800

    logging

--- server.js
+++ server.js
@@ -288,7 +288,7 @@ const recognizeStream = google_stt_client
 			}
 			else {verifAudio = Buffer.concat(msgBufd)}
 			console.log("auth for user ",voiceItUserId);
-		let authResult = doAuth(voiceItUserId, passphrase, verifAudio);
+		await authResult = doAuth(voiceItUserId, passphrase, verifAudio);
 		console.log("passing in an authresult of ",authResult);
 		processContentAuth(data.results[0].alternatives[0].transcript, authResult);
 		
